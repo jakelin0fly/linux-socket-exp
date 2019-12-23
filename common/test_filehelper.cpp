f4b677e11db42507155c0ebdb9c18f7a3e851f1b
JakeLin <jakelin0fly@gmail.com>
2019-12-23 20:24:43 +0800

    服务器信息读取测试

--- common/test_filehelper.cpp
+++ common/test_filehelper.cpp
@@ -1,19 +1,26 @@
-#include "filehelper.h"
+#include "helper.h"
 #include <vector>
 #include <iostream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	FileHelper::Serialize("data.txt", 3);
-	FileHelper::Serialize("data.txt", 56);
-	FileHelper::Serialize("data.txt", 8);
+	// Helper::Serialize("data.txt", 3);
+	// Helper::Serialize("data.txt", 56);
+	// Helper::Serialize("data.txt", 8);
 
-	std::vector<int> v;
-	FileHelper::Deserialize("data.txt", v);
+	// std::vector<int> v;
+	// Helper::Deserialize("data.txt", v);
+	// for (int i = 0; i < v.size(); ++i)
+	// {
+	// 	cout << v[i] << endl;
+	// }
+
+	std::vector<ServerStruct> v;
+	Helper::GetServersFromFile("config.txt", v);
 	for (int i = 0; i < v.size(); ++i)
 	{
-		cout << v[i] << endl;
+		cout<<v[i].str_ip<<":"<<v[i].port<<endl;
 	}
 
 	return 0;
